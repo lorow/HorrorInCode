c8cd8983c6c0a3dfdf13865e6b001500e4eeaec3
lorow <smykupyka@gmail.com>
2019-09-26 21:19:24 +0200

    Fix the bug with not reversible animation

--- HorrorInCode/frontend/src/components/ProjectsPanel/index.jsx
+++ HorrorInCode/frontend/src/components/ProjectsPanel/index.jsx
@@ -5,40 +5,44 @@ import './style.scss';
 export default function ProjectsPanel(props) {
 
   const [expanded, setExpanded] = useState(false);
+  
 
-  const handleImageResize = () =>{
-
-    var expandImageTimeline = anime.timeline({
+  const prepareAnimation = direction => {
+    let timeline = anime.timeline({
       duration: 1000,
       autoplay: false,
-      direction: 'normal',
-      loop: true,
+      direction: direction,
     })
   
-    expandImageTimeline.add({
-  
+    timeline.add({
+    
       duration:750,
       targets: '.project__cover',
       overflowX:'hidden',
   
-      width:'100vw',
-      height:'100vh',
+      width:['60vh', '300vh'],
+      height:['60vh', '100vh'],
       borderRadius:['100%','0%'],
-
+  
       easing: 'cubicBezier(0.620, -0.420, 0.390, 1.385)'
   
     }).add({
-  
       targets:'.project__title',
       color: ['rgba(255, 255, 255, 0.7)', 'rgba(255, 255, 255, 1)'],
-      top: '13vw',
+      top: ['37.9vh', '28vh'],
       
       easing: 'cubicBezier(0.315, 0.000, 0.330, 0.825)'
-  
     })
 
-    expandImageTimeline.reverse()
+    return timeline
+  }
+
+
+  const handleImageResize = () =>{
+
+    let expandImageTimeline = expanded ? prepareAnimation('reverse') : prepareAnimation('normal')
     expandImageTimeline.play()
+    expandImageTimeline.finished.then(() => {setExpanded(!expanded)})
   
   }
 
