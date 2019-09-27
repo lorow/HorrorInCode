241e692aac88409b61703c78a6a324c2e3638ba6
lorow <smykupyka@gmail.com>
2019-09-27 20:37:06 +0200

    Separate project slice from project container

--- HorrorInCode/frontend/src/containers/ProjectsPanelContainer/index.jsx
+++ HorrorInCode/frontend/src/containers/ProjectsPanelContainer/index.jsx
@@ -1,12 +1,12 @@
 import React, { useState }from 'react';
 import anime from 'animejs';
+import ProjectSlice from '../../components/ProjectSlice';
 import './style.scss';
 
 export default function ProjectsPanel(props) {
 
   const [expanded, setExpanded] = useState(false);
   
-
   const prepareAnimation = direction => {
     let timeline = anime.timeline({
       duration: 1000,
@@ -30,13 +30,10 @@ export default function ProjectsPanel(props) {
       targets:'.project__title',
       color: ['rgba(255, 255, 255, 0.7)', 'rgba(255, 255, 255, 1)'],
       top: ['37.9vh', '28vh'],
-      
       easing: 'cubicBezier(0.315, 0.000, 0.330, 0.825)'
     })
-
     return timeline
   }
-
 
   const handleImageResize = () =>{
 
@@ -48,25 +45,7 @@ export default function ProjectsPanel(props) {
 
   return (
     <section className="project__section">
-      <div className="image__container">
-        
-        <a className="project__button--previous" href="#previous-article">
-          <img className="project__icon--small" src="img/right-arrow.svg" alt="previous post" style={{transform: "rotate(180deg)"}}/>
-        </a>
-        
-        <h1 className="project__title">
-            FLUFFY BOT
-        </h1>
-
-        <div className="project__cover" onClick={handleImageResize}>
-          <img className="project__cover__image" src="https://i.pinimg.com/originals/b8/94/01/b89401be81d329ecb27280e34ef49627.jpg" alt="fluffy bot project cover"/>
-        </div>
-
-        <a className="project__button--next" href="#next-article">
-        <img className="project__icon--small project__icon--right" src="img/right-arrow.svg" alt="next post"/>
-        </a>
-
-      </div>
+      <ProjectSlice handleImageResize={handleImageResize}/>
     </section>
   );
 }
