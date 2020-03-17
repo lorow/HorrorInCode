2cafcb2ad9ce38b53f165195f7b96c12134531b8
lorow <smykupyka@gmail.com>
2020-03-17 16:30:23 +0100

    ADD single selects for articles and projects

--- HorrorInCode/API/schema.py
+++ HorrorInCode/API/schema.py
@@ -11,9 +11,21 @@ class ProjectType(DjangoObjectType):
 class ProjectQuery(object):
 
     projects = graphene.List(ProjectType)
+    project = graphene.Field(
+        ProjectType,
+        id=graphene.Int()
+    )
 
     def resolve_projects(self, info, **kwargs):
         return Project.objects.all()
+
+    def resolve_project(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is None:
+            return None
+
+        return Project.objects.get(pk=id)
 
 
 class TagType(DjangoObjectType):
@@ -30,9 +42,20 @@ class ArticleQuery(object):
 
     tags = graphene.List(TagType)
     articles = graphene.List(ArticleType)
+    article = graphene.Field(
+        ArticleType,
+        id=graphene.Int(),
+    )
 
     def resolve_articles(self, info, **kwargs):
         return Article.objects.all()
 
     def resolve_tags(self, info, **kwargs):
-        Tag.objects.all()
+        return Tag.objects.all()
+
+    def resolve_article(self, info, **kwargs):
+        id = kwargs.get('id')
+        if id is None:
+            return None
+
+        return Article.objects.get(pk=id)
