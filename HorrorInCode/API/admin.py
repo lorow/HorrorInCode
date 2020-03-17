f321eba701b9a04051158d6a3e7a317d1e6a0fb8
lorow <smykupyka@gmail.com>
2020-03-17 18:55:38 +0100

    Add venv, upgrade to py3.8, register models

--- HorrorInCode/API/admin.py
+++ HorrorInCode/API/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from API.models import Project, Tag, Article
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Tag)
+admin.site.register(Article)
