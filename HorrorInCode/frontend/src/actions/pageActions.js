c76dc610b6e7de28b6d1edf3e9c59e2dc6169913
lorow <smykupyka@gmail.com>
2020-04-06 12:09:46 +0200

    Move everything out of redundant HorrorInCode sub-dir

--- HorrorInCode/frontend/src/actions/pageActions.js
+++
@@ -1,9 +0,0 @@
-export const setBackgroundText = (text) => ({
-  type: "SET_BACKGROUND_TEXT",
-  payload: text
-})
-
-export const setVisualPageIndex = (index) => ({
-  type: "SET_VISUAL_PAGE_INDEX",
-  payload: index
-})
